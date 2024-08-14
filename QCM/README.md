88d3541ef453728fa0ffaca9005e1573c0e21aa2
Stacy Druart <stacy@StacyDruartMSI>
2024-08-14 10:46:45 +0200

    ajout random

--- QCM/README.md
+++ QCM/README.md
@@ -9,6 +9,31 @@ Clonez le dépôt avec la commande suivante :
 git clone git@github.com:Druart-Stacy/QcmProgramation2.git
 cd QcmProgramation2
 
+## instalation du projet
+Assurez-vous d'avoir Node.js et npm installés, puis exécutez :
+npm install
+demarrer l'aplication
+npm start
+
+L'application sera disponible à l'adresse http://localhost:3000.
+
+Structure du Code
+src/App.js : Composant principal qui gère la logique du quiz et l'affichage des questions.
+src/Question.js : Composant de présentation pour afficher une question et ses options.
+src/App.css : Fichier de styles CSS pour le composant App.
+src/img/ : Répertoire contenant les images utilisées pour afficher les résultats.
+
+## Configuration des Questions
+Les questions du quiz sont définies dans le tableau questions dans le fichier App.js. Vous pouvez ajouter, supprimer ou modifier les questions selon vos besoins.
+
+## Contribuer
+Les contributions sont les bienvenues ! Si vous souhaitez améliorer l'application ou corriger des bugs, veuillez suivre ces étapes :
+
+Forker le dépôt.
+Créer une nouvelle branche (git checkout -b feature/ma-nouvelle-fonctionnalite).
+Apporter vos modifications.
+Soumettre une pull request.
+
 ## Première version :
 
 une question par page avec un choix parmi 3 propositions.
@@ -18,7 +43,7 @@ Score final affiché sur 50.
 
 Page de questions avec un choix parmi 3 propositions.
 Score final affiché sur 50.
-Page d'accueil.
+randomisation des questions.
 
 ## Langages et framework utilisés :
 
--- QCM/src/App.js
+++ QCM/src/App.js
@@ -1,21 +1,281 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About'; // Exemple d'autre composant
+// App.js
+import React, { useState, useEffect } from 'react';
+import Question from './Question';
+import './App.css';
+import tristeImage from './img/larmeleon.png';
+import felicitationImage from './img/mackognieur.png';
+import legendeImage from './img/arceus.jpg';
+import bonnemoyenne from'./img/porigon2.jpg';
 
-const Question = () => {
+// Définition des questions du quiz
+// Questions
+const questions = [
+  {
+    questionText: "Qu'est-ce que JavaScript ?",
+    options: [
+      'A. Un langage de programmation', 
+      'B. Une marque de GSM', 
+      'C. Une ville'
+    ],
+    correctAnswer: 'A. Un langage de programmation',
+  },
+  {
+    questionText: "Qu'est-ce que Java ?",
+    options: [
+      'A. Une marque de café', 
+      'B. Un langage de programmation', 
+      'C. La réponse C'
+    ],
+    correctAnswer: 'B. Un langage de programmation',
+  },
+  {
+    questionText: 'Comment déclare-t-on une variable en JavaScript ?',
+    options: [
+      'A. Nom de la variable', 
+      'B. Avec le symbole $', 
+      'C. Avec "let" ou "const"'
+    ],
+    correctAnswer: 'C. Avec "let" ou "const"',
+  },
+  {
+    questionText: 'Quelle est la différence entre "let" et "const" ?',
+    options: [
+      'A. "let" est utilisé pour déclarer une variable locale à un bloc d’instructions. "const" est utilisé pour déclarer une variable constante qui ne peut pas être réaffectée.', 
+      'B. "const" est utilisé pour déclarer une variable locale à un bloc d’instructions. "let" est utilisé pour déclarer une variable constante qui ne peut pas être réaffectée.',
+      'C. Aucune différence entre les deux'
+    ],
+    correctAnswer: 'A. "let" est utilisé pour déclarer une variable locale à un bloc d’instructions. "const" est utilisé pour déclarer une variable constante qui ne peut pas être réaffectée.',
+  },
+  {
+    questionText: 'Comment utiliser des commentaires en JavaScript ?',
+    options: [
+      'A. /**/',  
+      'B. //',
+      'C. --'
+    ],
+    correctAnswer: 'B. //',
+  },
+  {
+    questionText: 'Comment effectuer une boucle while en JavaScript ?',
+    options: [
+      'A. while (condition)\n 	// instruction',  
+      'B. while (condition) {\n    // instructions à répéter\n}',
+      'C. while condition\n // instructions'
+    ],
+    correctAnswer: 'B. while (condition) {\n    // instructions à répéter\n}',
+  },
+  {
+    questionText: "Quel framework n’est pas un framework JavaScript ?",
+    options: [
+      'A. Angular', 
+      'B. Laravel', 
+      'C. ReactJS'
+    ],
+    correctAnswer: 'B. Laravel',
+  },
+  {
+    questionText: 'Que veut dire langage de haut niveau ?',
+    options: [
+      'A. Cela signifie que c’est un langage plus près du langage humain', 
+      'B. Cela signifie que c’est un langage difficile à apprendre', 
+      'C. Cela signifie que c’est un langage plus éloigné du langage humain'
+    ],
+    correctAnswer: 'A. Cela signifie que c’est un langage plus près du langage humain',
+  },
+  {
+    questionText: 'Que veut dire SEM ?',
+    options: [
+      'A. Structural Equation Modeling', 
+      'B. Scanning Electron Microscope', 
+      'C. Search Engine Marketing'
+    ],
+    correctAnswer: 'C. Search Engine Marketing',
+  },
+  {
+    questionText: 'Quels sont les langages de haut niveau parmi ceux-ci ?',
+    options: [
+      'A. Python', 
+      'B. Ruby', 
+      'C. Assembly'
+    ],
+    correctAnswer: ['A. Python', 'B. Ruby'],
+  },
+  {
+    questionText: 'Que signifie include en PHP ?',
+    options: [
+      'A. Importer le contenu d’un fichier dans un script courant',
+      'B. Exporter le contenu d’un fichier dans un script courant',
+      'C. C’est une balise HTML'
+    ],
+    correctAnswer: 'A. Importer le contenu d’un fichier dans un script courant',
+  },
+  {
+    questionText: "Lequel de ces langages est Backend ?",
+    options: [
+      'A. Java', 
+      'B. JavaScript', 
+      'C. CSS'
+    ],
+    correctAnswer: 'A. Java',
+  },
+  {
+    questionText: 'Quelle est la différence entre HTML, CSS et JavaScript ?',
+    options: [
+      'A. HTML et JavaScript sont des langages de balisage tandis que CSS est un langage de programmation', 
+      'B. JavaScript et CSS sont des langages de balisage tandis que HTML est un langage de programmation', 
+      'C. HTML et CSS sont des langages de balisage tandis que JavaScript est un langage de programmation'
+    ],
+    correctAnswer: 'C. HTML et CSS sont des langages de balisage tandis que JavaScript est un langage de programmation',
+  },
+  {
+    questionText: 'Quelle est la différence entre SEO et SEA ?',
+    options: [
+      'A. Le SEO est la publicité payante sur les moteurs de recherche, tandis que le SEA est l’optimisation des contenus pour les moteurs de recherche.', 
+      'B. Le SEO est l’optimisation d’un site web pour les moteurs de recherche, visant à améliorer son classement dans les résultats naturels (organiques). Le SEA concerne les annonces payantes qui apparaissent en premier dans les résultats des moteurs de recherche.', 
+      'C. Le SEO et le SEA sont deux termes qui désignent les techniques pour améliorer la performance des sites web sur les réseaux sociaux.'
+    ],
+    correctAnswer: 'B. Le SEO est l’optimisation d’un site web pour les moteurs de recherche, visant à améliorer son classement dans les résultats naturels (organiques). Le SEA concerne les annonces payantes qui apparaissent en premier dans les résultats des moteurs de recherche.',
+  },
+  {
+    questionText: 'Quelle est la différence entre les méthodes GET et POST dans une requête HTTP ?',
+    options: [
+      'A. GET : Utilisée uniquement pour télécharger des fichiers depuis un serveur. Les données ne peuvent pas être envoyées avec cette méthode. POST : Utilisée uniquement pour télécharger des images depuis un serveur. Les données ne peuvent pas être envoyées avec cette méthode.', 
+      'B. GET : Utilisée pour envoyer des données au serveur, souvent pour soumettre des formulaires. Les paramètres sont envoyés dans le corps de la requête. POST : Utilisée pour récupérer des données d’un serveur. Les paramètres sont envoyés dans l’URL de la requête.', 
+      'C. GET : Utilisée pour récupérer des données d’un serveur. Les paramètres sont envoyés dans l’URL de la requête. POST : Utilisée pour envoyer des données au serveur, souvent pour soumettre des formulaires. Les paramètres sont envoyés dans le corps de la requête.'
+    ],
+    correctAnswer: 'C. GET : Utilisée pour récupérer des données d’un serveur. Les paramètres sont envoyés dans l’URL de la requête. POST : Utilisée pour envoyer des données au serveur, souvent pour soumettre des formulaires. Les paramètres sont envoyés dans le corps de la requête.',
+  },
+  {
+    questionText: 'Quelle est la différence entre une base de données SQL et NoSQL ?',
+    options: [
+      'A. SQL : Utilise des fichiers texte pour stocker les données et ne nécessite pas de langage de requête particulier. Exemples : JSON, XML. NoSQL : Utilise uniquement des tables structurées avec des lignes et des colonnes, tout comme les bases de données SQL. Exemples : MySQL, PostgreSQL.', 
+      'B. SQL : Utilise des tables structurées avec des lignes et des colonnes et le langage SQL pour gérer les données. Exemples : MySQL, PostgreSQL. NoSQL : Utilise des structures de données flexibles comme les documents, les graphes, les colonnes, ou les clés-valeurs. Exemples : MongoDB, Cassandra.',
+      'C. SQL : Utilise un langage de programmation comme Python pour gérer les données. Exemples : SQLite, Oracle. NoSQL : Utilise des fichiers audio pour stocker les données. Exemples : MP3, WAV.'
+    ],
+    correctAnswer: 'B. SQL : Utilise des tables structurées avec des lignes et des colonnes et le langage SQL pour gérer les données. Exemples : MySQL, PostgreSQL. NoSQL : Utilise des structures de données flexibles comme les documents, les graphes, les colonnes, ou les clés-valeurs. Exemples : MongoDB, Cassandra.',
+  },
+];
+
+
+const App = () => {
+  // États pour gérer la question courante, le score, l'affichage du résultat, le statut de la réponse, et les réponses sélectionnées
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [answerStatus, setAnswerStatus] = useState('');
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+
+  // Randomisation des questions au chargement du composant
+  useEffect(() => {
+    shuffleQuestions();
+  }, []);
+
+  // Fonction pour mélanger les questions
+  const shuffleQuestions = () => {
+    questions.sort(() => Math.random() - 0.5);
+  };
+
+  // Fonction pour gérer la réponse de l'utilisateur
+  const handleAnswer = (answer) => {
+    const question = questions[currentQuestion];
+
+    if (Array.isArray(question.correctAnswer)) {
+      // Si la réponse correcte est un tableau (réponse multiple)
+      if (selectedAnswers.includes(answer)) {
+        setSelectedAnswers(selectedAnswers.filter(item => item !== answer)); // Décocher l'option si déjà sélectionnée
+      } else {
+        setSelectedAnswers([...selectedAnswers, answer]); // Ajouter l'option si sélectionnée
+      }
+    } else {
+      // Réponse simple
+      if (answer === question.correctAnswer) {
+        setScore(score + 1);
+        setAnswerStatus('Correct!');
+      } else {
+        setAnswerStatus('Incorrect');
+      }
+      goToNextQuestion();
+    }
+  };
+
+  // Fonction pour vérifier les réponses multiples sélectionnées
+  const checkMultipleAnswers = () => {
+    const question = questions[currentQuestion];
+    const correctAnswers = question.correctAnswer;
+
+    // Vérification que toutes les réponses sélectionnées sont correctes
+    const isCorrect = selectedAnswers.every(answer => correctAnswers.includes(answer)) && 
+                      selectedAnswers.length === correctAnswers.length;
+
+    if (isCorrect) {
+      setScore(score + 1);
+      setAnswerStatus('Correct!');
+    } else {
+      setAnswerStatus('Incorrect');
+    }
+
+    goToNextQuestion();
+  };
+
+  // Fonction pour passer à la question suivante ou afficher les résultats
+  const goToNextQuestion = () => {
+    const nextQuestion = currentQuestion + 1;
+    setSelectedAnswers([]); // Réinitialiser les réponses sélectionnées
+
+    if (nextQuestion < questions.length) {
+      setTimeout(() => {
+        setCurrentQuestion(nextQuestion);
+        setAnswerStatus('');
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        setShowResult(true);
+      }, 1000);
+    }
+  };
+
+  // Fonction pour afficher l'image correspondant au score final
+  const renderResult = () => {
+    const scoreRatio = score / questions.length;
+    if (scoreRatio === 1.0) {
+      return <img src={legendeImage} alt="Légende" />;
+    } 
+    else if (scoreRatio >=0.7){
+      return <img src={felicitationImage} alt="Félicitations" />;
+    } 
+    else if (scoreRatio >= 0.5) {
+      return <img src={bonnemoyenne} alt="bonne moyenne" />;
+    } else {
+      return <img src={tristeImage} alt="Triste" />;
+    }
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        {/* Ajoutez d'autres routes ici */}
-      </Routes>
-    </Router>
+    <div className="App">
+      {showResult ? (
+        <div className="result">
+          <h1>Votre score est {score} sur {questions.length}</h1>
+          {renderResult()}
+        </div>
+      ) : (
+        <div>
+          <Question
+            question={questions[currentQuestion].questionText}
+            options={questions[currentQuestion].options}
+            onAnswer={handleAnswer}
+          />
+          {Array.isArray(questions[currentQuestion].correctAnswer) && (
+            <button 
+              onClick={checkMultipleAnswers}
+              disabled={selectedAnswers.length !== questions[currentQuestion].correctAnswer.length}>
+              Valider les réponses multiples
+            </button>
+          )}
+        </div>
+      )}
+      <div className="status">{answerStatus}</div>
+    </div>
   );
 };
 
-export default Question;
-
-
+export default App;
